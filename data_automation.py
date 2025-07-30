694b71156c6330742ae6f7ddf677453f8bfdd4b5
Fawwaz Atha Rohmatullah <132468764+fawazatha@users.noreply.github.com>
2025-07-30 16:19:03 +0700

    Update scraping indices zip and push script

--- data_automation.py
+++ data_automation.py
@@ -1,20 +1,32 @@
+from supabase   import create_client
+from dotenv     import load_dotenv
+
 import pandas as pd
 import zipfile
 import os
-from supabase import create_client
-from dotenv import load_dotenv
+
 
 load_dotenv()
 
 # Fetch company name data from idx_company_profile data in db
-url_supabase = os.environ.get("SUPABASE_URL")
-key = os.environ.get("SUPABASE_KEY")
-supabase = create_client(url_supabase, key)
+URL_SUPABASE = os.environ.get("SUPABASE_URL")
+KEY_SUPABASE = os.environ.get("SUPABASE_KEY")
+SUPABASE_CLIENT = create_client(URL_SUPABASE, KEY_SUPABASE)
 
-idx_data = supabase.table("idx_company_profile").select("symbol","company_name").execute()
-idx_data = pd.DataFrame(idx_data.data)
+IDX_DATA = SUPABASE_CLIENT.table("idx_company_profile").select("symbol","company_name").execute()
+IDX_DATA = pd.DataFrame(IDX_DATA.data)
 
-def delete_all_files(directory_path):
+
+def delete_all_files(directory_path: str) -> str:
+    """ 
+    Deletes all files in the specified directory.
+
+    Args:
+        directory_path (str): Path to the directory from which files will be deleted.
+    
+    Returns:
+        str: Confirmation message indicating that all files have been deleted.
+    """
     # List all files in the directory
     for filename in os.listdir(directory_path):
         file_path = os.path.join(directory_path, filename)
@@ -31,7 +43,14 @@ def delete_all_files(directory_path):
     return(f"All files in {directory_path} have been deleted.")
 
 
-def unzip_file(file_directory, extract_directory):
+def unzip_file(file_directory: str, extract_directory: str):
+    """ 
+    Unzips all zip files in the specified directory to the extract directory.
+
+    Args:
+        file_directory (str): Directory containing the zip files.
+        extract_directory (str): Directory where the files will be extracted.
+    """
     # Get list of files in the specified directory
     files = os.listdir(file_directory)
 
@@ -44,8 +63,18 @@ def unzip_file(file_directory, extract_directory):
     
     return print(f"Finish Unzip file from {file_directory} folder into {extract_directory} folder")
 
-def company_list_extraction(data_path,idx_data):
 
+def company_list_extraction(data_path: str, idx_data: pd.DataFrame) -> pd.DataFrame:
+    """ 
+    Extracts company list from the provided Excel file and merges it with idx_data.
+
+    Args:
+        data_path (str): Path to the Excel file containing company data.
+        idx_data (pd.DataFrame): DataFrame containing idx company profile data.
+    
+    Returns:
+        pd.DataFrame: DataFrame containing the extracted company symbols.
+    """
     df = pd.read_excel(data_path, skiprows=7).iloc[1:,2:]
 
     # Convert the column to numeric, setting errors='coerce' will convert non-numeric values to NaN
@@ -64,29 +93,42 @@ def company_list_extraction(data_path,idx_data):
 
     return df
 
-# Unzip file
-unzip_file("source_data", "source_data/extracted_data")
 
-# Extract Symbol
-files = os.listdir("source_data/extracted_data")
+def run_indices_update():
+    """ 
+    Main function to run the indices update process.
+    Unzips the downloaded files, extracts company lists, and saves them.
+    """
+    # Unzip file
+    unzip_file("source_data", "source_data/extracted_data")
 
-indices_list = ["IDX30",'LQ45','KOMPAS100', "IDX BUMN20",'IDX HIDIV20', 'IDX G30','IDX V30', "IDX Q30", "IDX ESGL", "SRIKEHATI", "SMINFRA18", "JII70", "ECONOMIC30"]
+    # Extract Symbol
+    files = os.listdir("source_data/extracted_data")
 
-# Filter only zip files
-excel_files = [f for f in files if os.path.isfile(os.path.join("source_data/extracted_data", f)) and f.endswith('.xlsx')]
+    indices_list = ["IDX30",'LQ45','KOMPAS100', "IDX BUMN20",'IDX HIDIV20', 'IDX G30','IDX V30', "IDX Q30", "IDX ESGL", "SRIKEHATI", "SMINFRA18", "JII70", "ECONOMIC30"]
 
-for i in indices_list:
-    try:
-        file = [s for s in excel_files if i in s.upper()][0]
+    # Filter only zip files
+    excel_files = [f for f in files if os.path.isfile(os.path.join("source_data/extracted_data", f)) and f.endswith('.xlsx')]
 
-        data = company_list_extraction(f"source_data/extracted_data/{file}",idx_data)
+    # Make sure company list dir
+    os.makedirs("company_list", exist_ok=True)
 
-        data.to_csv(f"company_list/companies_list_{i.lower().replace(' ','')}.csv", index=False)
+    for indices in indices_list:
+        try:
+            file = [s for s in excel_files if indices in s.upper()][0]
 
-        print(f"Company list for {i} index already extracted")
-    except:
-       print(f"No new update for {i} indices")
+            data = company_list_extraction(f"source_data/extracted_data/{file}", IDX_DATA)
 
-print("----------------------------------------------------------")
+            data.to_csv(f"company_list/companies_list_{indices.lower().replace(' ','')}.csv", index=False)
 
-delete_all_files("source_data/extracted_data")
+            print(f"Company list for {indices} index already extracted")
+        except:
+            print(f"No new update for {indices} indices")
+
+    print("----------------------------------------------------------")
+
+
+if __name__ == '__main__':
+    run_indices_update()
+    delete_all_files("source_data/extracted_data")
+    delete_all_files("source_data")
---
+++ indices_zip_data_scraper.py
@@ -0,0 +1,148 @@
+from dotenv     import load_dotenv 
+from datetime   import datetime
+
+import os 
+import logging 
+import ssl
+import urllib.request
+import json 
+import time 
+import random 
+
+
+# Setup Logging
+logging.basicConfig(
+    filename='indices_scraping.log', # Set a file for save logger output 
+    level=logging.INFO, # Set the logging level
+    format='%(asctime)s [%(levelname)s] - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+    )
+LOGGER = logging.getLogger(__name__)
+LOGGER.info("Init Global Variable")
+
+
+# Setup .env
+load_dotenv(override=True)
+PROXY = os.getenv('PROXY')
+
+
+class ProxyRequester:
+    def __init__(self, proxy=None):
+        """Initializes the ProxyRequester class with the provided proxy
+
+        Args:
+            proxy (str, optional): the proxy to be used. Defaults to None. Example: 'brd-customer-xxx-zone-xxx:xxx@brd.superproxy.io:xxx'
+        """
+
+        # Set up SSL context to unverified
+        ssl._create_default_https_context = ssl._create_unverified_context
+
+        proxy_support = urllib.request.ProxyHandler({'http': proxy,'https': proxy})
+        opener = urllib.request.build_opener(proxy_support)
+        urllib.request.install_opener(opener)
+
+    def fetch_url(self, url) -> str:
+        """
+        Fetches the content of a URL using the installed opener.
+        
+        Args:
+            url (str): The URL to fetch.
+        
+        Returns:
+            str: The content of the URL as a string, or False if an error occurs.
+        """
+        # Use the installed opener to fetch the URL
+        try:
+            with urllib.request.urlopen(url) as response:
+                return response.read().decode()
+        except Exception as error:
+            print(f"Error fetching URL: {error}")
+            return False
+    
+    def download_file(self, url: str, saved_dir: str) -> bool:
+        """
+        Downloads a file from the specified URL and saves it to the given directory.
+        
+        Args:
+            url (str): The URL of the file to download.
+            saved_dir (str): The directory where the file will be saved.
+        
+        Returns:
+            bool: True if the file was downloaded and saved successfully, False otherwise.
+        """
+        try: 
+            with urllib.request.urlopen(url) as response:
+                file_content = response.read()
+
+            with open(saved_dir, 'wb') as file_zip:
+                file_zip.write(file_content)
+            
+            print(f"Zip files saved to: {saved_dir}")
+            return True
+        except Exception as error:
+            print(f"Failed to download {url}. Error: {error}")
+            return False
+
+
+# Requester api url 
+REQUESTER = ProxyRequester(proxy=PROXY)
+
+
+def get_zip_files():
+    """ 
+    Downloads zip files from the IDX website for various indices and saves them to a local directory.
+    """
+    indices_list = ["IDX30",'LQ45','KOMPAS100', "IDX BUMN20",'IDX HIDIV20', 'IDX G30','IDX V30', "IDX Q30", "IDX ESGL", "SRIKEHATI", "SMINFRA18", "JII70", "ECONOMIC30"]
+    current_year = datetime.now().year
+    base_url = "https://www.idx.co.id"
+
+    save_dir = "source_data"
+    os.makedirs(save_dir, exist_ok=True)
+
+    # Loop through each index and fetch the corresponding data
+    for indices in indices_list:
+        encoded_indices = indices.replace(' ','')
+        
+        LOGGER.info(f"Fetching data for index: {indices} for year {current_year}")
+
+        # Construct the API URL for the current index
+        api_url = f"https://www.idx.co.id/secondary/get/StockData/GetStockUploader?typeIndex={encoded_indices}&year={current_year}&table=stockIndex&locale=id"
+        response = REQUESTER.fetch_url(api_url)
+        if response == False:
+             raise Exception("Error accesing api url.") 
+
+        # Parse the JSON response
+        datas = json.loads(response)
+        
+        # Get data result 
+        data_results = datas.get('Results')
+        if not data_results:
+            LOGGER.info(f"No data found for index: {indices}. Skipping.")
+            continue
+
+        # Extract latest file
+        latest_result = data_results[0]
+       
+        # Define the filename and full local path for saving
+        original_file_name = latest_result.get('AttachmentName')
+        _, extension = os.path.splitext(original_file_name) 
+        new_file_name = f"index_data_idx_downloaded_{indices}{extension}"
+        local_file_path = os.path.join(save_dir, new_file_name)
+
+        # Proceed with download for new file
+        url_to_download = latest_result.get('AttachmentUrl')
+        full_url = base_url + url_to_download
+        LOGGER.info(f"Downloading file: {full_url}")
+
+        success = REQUESTER.download_file(full_url, local_file_path)
+        
+        if success:
+            LOGGER.info(f"Successfully saved to {local_file_path}")
+            # Random sleep to avoid hitting the server too hard
+            random_sleep = random.uniform(3, 12)
+            time.sleep(random_sleep)
+
+
+if __name__ == '__main__':
+    get_zip_files()
+
